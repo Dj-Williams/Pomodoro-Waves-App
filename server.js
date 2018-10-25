cfa26963ceedd512bd0a7974229fe4aaaa96828d
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-10-25 17:51:48 -0400

    Got my server up and running!

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', routes)
 
+app.set('view engine', 'hbs')
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Yo! Your server is actually listening to you on PORT: ${PORT}`)
