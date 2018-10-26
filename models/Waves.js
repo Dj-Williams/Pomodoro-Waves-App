ced7bdc00ea75c78a3aeb61b723e054a7dcd2f67
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-10-26 13:19:49 -0400

    Completed songs and Waves models, working on User model

---
+++ models/Waves.js
@@ -0,0 +1,16 @@
+const mongoose = require('../db/connections')
+const Schema = mongoose.Schema
+
+const Waves = new Schema({
+    wave_Name: String,
+    backgroundImg: String,
+    description: String,
+    Songs: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Songs'
+        }
+    ]
+})
+
+module.exports = mongoose.model('Waves', Waves)
