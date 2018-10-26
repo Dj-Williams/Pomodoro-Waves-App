11a857bb708c10c1fe6927fba94989123b7fede9
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-10-26 13:24:50 -0400

    Completed user model

--- models/User.js
+++ models/User.js
@@ -4,4 +4,13 @@ const Schema = mongoose.Schema
 const User = new Schema({
     username: String,
     password: String,
+    favGenre: String,
+    Age: Number,
+    profilePic: String,
+    waves: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Waves'
+    }]
 })
+
+module.exports = mongoose.model('User', User)
