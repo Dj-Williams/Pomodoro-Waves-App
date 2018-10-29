34c848bcf7f4e7c0f088cfb205055abf7f3aa51b
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-10-29 13:58:51 -0400

    Imported dotenv and added the mongodb_uri to the connections

--- db/connections.js
+++ db/connections.js
@@ -1,6 +1,15 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
+// connecting to the database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
 mongoose.connect('mongodb://localhost/pomodoro-waves')
+}
+
+
 
 mongoose.connection.once('open', () => {
     console.log ('Mongoose is now connected to MongoDb, yo!')
